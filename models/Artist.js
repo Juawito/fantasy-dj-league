34e1b6332337a65e3f7eddfc8965e031c5eaed1d
Mikey Hauser <mikeyhauser@gmail.com>
2021-05-08 10:43:57 -0400

    commit for handlebars proto

---
+++ models/Artist.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Artist extends Model {}
+
+Artist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    artist: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    track: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    album: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    null: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'artist',
+  }
+);
+
+module.exports = Artist;
