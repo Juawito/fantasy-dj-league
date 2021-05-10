c44a1ebb1081b51f2cdbb6f6b2f2a852224ef140
Juawito <santiagojuan311@gmail.com>
2021-05-10 19:08:06 -0400

    added functionality for heroku

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,29 @@
-const router = require('express').router();
-const path = require('path');
+const router = require('express').Router();
+const {User, Playlist, Player} = require('../models');
+const withAuth = require('../utils/withAuth')
 
 router.get('/', async (req, res) => {
     res.render('homepage');
 })
+router.get('login', async (req, res) => {
+    res.render('login');
+})
+// router.get('/user', async (req, res) => {
+//     try {
+//         const userData = await User.findAll({
+//             where: {
+//                 userName = req.body.userName,
+//                 password = req.body.password
+//             }
+//         })
+//         const topPlaylists = await Playlist.findAll();
+        
+
+//         res.render('userHome', { userData , topTwo});
+//     } catch (error) {
+//         if (error) throw error;
+//         res.status(500).json(error);
+//     }
+// })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers});
+const hbs = exphbs.create({});
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -17,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public/')));
 
-app.use(require('./controllers/artist-routes'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
