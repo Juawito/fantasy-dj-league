fd4375d45490cb353e748183d088542915e4f8c8
Juawito <santiagojuan311@gmail.com>
2021-05-15 11:56:54 -0400

    added put route to increase playlist count

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,48 @@
 const axios = require('axios').default;
 
-const signInButton = document.querySelector('.sign-in-button')
+const signUpNav = document.querySelector("#signUpNav");
+const signInNav = document.querySelector("#signInNav");
+const logOffNav = document.querySelector("#logOffNav");
+const userPageNav = document.querySelector("#userPageNav");
+const playoffsNav = document.querySelector("#playoffsNav");
+const like = document.querySelector("#like");
+function signUpNavF()
+{
+    console.log("singup click");
+}
+function signInNavF()
+{
+    console.log("in click");
+}
+const logOffNavF = async() =>{
+    const response = await axios({
+        method: 'post',
+        url: '/api/user/logout',
+        headers: { 'Content-Type': 'application/json'},
+    })
 
+    if(response.statusText === 'ok'){
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+    console.log("off click");
+}
+function userPageNavF()
+{
+    console.log("user click");
+}
+function playoffsNavF()
+{
+    console.log("playoff click");
+}
+function likeF()
+{
+    console.log("like click");
+}
+signUpNav.addEventListener("click", signUpNavF);
+signInNav.addEventListener("click", signInNavF);
+logOffNav.addEventListener("click", logOffNavF);
+userPageNav.addEventListener("click", userPageNavF);
+playoffsNav.addEventListener("click", playoffsNavF);
+like.addEventListener("click", likeF);
--- public/js/login.js
+++ public/js/login.js
@@ -18,9 +18,40 @@ const loginFormHandler = async (event) => {
 
         if (response.statusText === 'ok') {
             document.location.replace('/profile');
-        }else {
-            alert (response.statusText)
+        } else {
+            alert(response.statusText)
         }
     }
 }
 
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+    const newUsername = document.querySelector('#new-username').value.trim();
+    const newUserEmail = document.querySelector('#new-userEmail').value.trim();
+    const newUserPassword = document.querySelector('#new-userPass').value.trim();
+    const repeatedPassword = document.querySelector('#password-repeat').value.trim();
+
+    if(!newUserPassword === repeatedPassword) {
+        alert('Passwords do not match please try again');
+        newUserPassword = '';
+        newUserEmail = '';
+        newUsername = '';
+        repeatedPassword = '';
+    } else if ( newUsername && newUserEmail && newUserPassword) {
+        const response = await axios({
+            method: 'post',
+            url: '/api/user/add',
+            body: JSON.stringify({ newUsername, newUserPassword, newUserEmail }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+         if (response.statusText === 'ok') {
+             document.location.replace('/profile');
+         } else {
+             alert(response.statusText);
+         }
+    }
+}
+
+
+signInBtn.addEventListener('submit', loginFormHandler);
+signUpBtn.addEventListener('submit', signupFormHandler);
