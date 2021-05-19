6cf4e3340249a5fa690482c8227720a65cb3cce2
Juawito <santiagojuan311@gmail.com>
2021-05-19 18:15:20 -0400

    fixing async await error for heroku

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,7 @@ const mysql = require('mysql');
 router.get('/', async (req, res) => {
     try {
         res.render('landing');
-        res.status(200);``
+        res.status(200); ``
     } catch (error) {
         if (error) throw error;
         res.status(500).json(error);
@@ -26,7 +26,7 @@ router.get('/login', async (req, res) => {
             res.render('login').status(200);
         }
     } catch (error) {
-        if(error) throw error;
+        if (error) throw error;
         console.log(error);
     }
 })
@@ -41,57 +41,52 @@ router.get('/signup', async (req, res) => {
 })
 router.get('/profile', withAuth, async (req, res) => {
     try {
-        if (req.session.logged_in) {
-            await selectPlaylist();
-            async function selectPlaylist() {
-                let [user, top1Metadata] = await sequelize.query(`SELECT id, artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = "${req.session.user_id}";`);
-                let [userList, top1RMetadata] = await sequelize.query("SELECT artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = 2;");
-                let [results2, metadata2] = await sequelize.query(`SELECT playlist_name, count, id FROM fantasy_dj_db.playlist ORDER BY count DESC LIMIT 2`);
-                // console.log(results2)
+        selectPlaylist();
+        async function selectPlaylist() {
+            let [user, top1Metadata] = await sequelize.query(`SELECT id, artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = "${req.session.user_id}";`);
+            let [userList, top1RMetadata] = await sequelize.query("SELECT artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = 2;");
+            let [results2, metadata2] = await sequelize.query(`SELECT playlist_name, count, id FROM fantasy_dj_db.playlist ORDER BY count DESC LIMIT 2`);
+            // console.log(results2)
 
 
-                // these are the names of the playlists
-                let top1PlaylistName = results2[0];
-                let top2PlaylistName = results2[1].playlistName;
+            // these are the names of the playlists
+            let top1PlaylistName = results2[0];
+            let top2PlaylistName = results2[1].playlistName;
 
-                //these are the ids of the playlists, plug these into the next sql searches to bring up the players
-                let top1Playlist = results2[0].id;
-                let top2Playlist = results2[1].id;
-                // console.log(top1Playlist, top2Playlist)
-                // these sql searches pull up each player in playlists
-                let [top1Res, top1ResMetadata] = await sequelize.query(`SELECT id, artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = "${top1Playlist}";`);
-                console.log(top1Res)
-                // console.log(top1PlaylistName)
+            //these are the ids of the playlists, plug these into the next sql searches to bring up the players
+            let top1Playlist = results2[0].id;
+            let top2Playlist = results2[1].id;
+            // console.log(top1Playlist, top2Playlist)
+            // these sql searches pull up each player in playlists
+            let [top1Res, top1ResMetadata] = await sequelize.query(`SELECT id, artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = "${top1Playlist}";`);
+            console.log(top1Res)
+            // console.log(top1PlaylistName)
 
 
 
-                let [top2Res, top2ResMetadata] = await sequelize.query(`SELECT artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = "${top2Playlist}";`);
-                console.log(top2Res)
-                // console.log(top2PlaylistName)
+            let [top2Res, top2ResMetadata] = await sequelize.query(`SELECT artist, track, playlist_id FROM fantasy_dj_db.player WHERE playlist_id = "${top2Playlist}";`);
+            console.log(top2Res)
+            // console.log(top2PlaylistName)
 
-                // console.log(userList)
-                // // connection.end
-                res.render('userinfo', { userList, top1Res, top2Res, top1PlaylistName, top2PlaylistName, logged_in: true });
-            }
+            // console.log(userList)
+            // // connection.end
+            res.render('userinfo', { userList, top1Res, top2Res, top1PlaylistName, top2PlaylistName, logged_in: true });
 
-        } else {
-            res.redirect('/');
         }
-
     } catch (error) {
         if (error) throw error;
         res.status(404).json(error);
     }
 
 })
-router.get('/all', withAuth,  async (req, res) => {
+router.get('/all', withAuth, async (req, res) => {
     try {
         // await selectAllPlaylist()
         // async function selectAllPlaylist() {
         //     let [allPlaylists, metadata2] = await sequelize.query(`SELECT id, playlist_name, count  FROM fantasy_dj_db.playlist ORDER BY count DESC`);
         //     res.render('allplayers', { allPlaylists });
         // }
-        const playlists= await Playlist.findAll({
+        const playlists = await Playlist.findAll({
             // include: [
             //     {
             //         model: Player,
@@ -105,9 +100,9 @@ router.get('/all', withAuth,  async (req, res) => {
                 ['id', 'DESC'],
             ]
         });
-        const allPlaylists = playlists.map((playlist) => playlist.get({ plain: true}));
+        const allPlaylists = playlists.map((playlist) => playlist.get({ plain: true }));
         console.log(allPlaylists);
-        res.render('allplayers', {allPlaylists });
+        res.render('allplayers', { allPlaylists });
     } catch (error) {
         if (error) throw error;
         res.status(500);
